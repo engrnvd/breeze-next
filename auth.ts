71aaaf5529f0a6185b2368f1922b14a25ab73756
Naveed Hassan <08es34@gmail.com>
2024-10-10 12:18:13 +0500

    init next auth

---
+++ auth.ts
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // E.g., domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null
+
+        // logic to salt and hash password
+        const pwHash = credentials.password
+
+        // logic to verify if the user exists
+        user = await new Promise(resolve => setTimeout(() => {
+          resolve({ email: credentials.email, password: pwHash })
+        }, 500))
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error('User not found.')
+        }
+
+        return user
+      },
+    }),
+  ],
+})
