198820942bdb1002bf4152995142bb0afb8f11bf
Naveed Hassan <08es34@gmail.com>
2024-10-16 17:51:51 +0500

    added support for csrf key

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -5,10 +5,14 @@ import { auth } from '@/auth'
 export async function apiFetch(uri: string, config: RequestInit = {}) {
   const url = process.env.BACKEND_URL + uri
   const session = await auth()
+
+  if (!process.env.BREEZE_NEXT_CSRF_KEY) throw new Error('BREEZE_NEXT_CSRF_KEY is missing in your .env file. Please visit https://github.com/engrnvd/laravel-breeze-next for details.')
+
   const headers: HeadersInit = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
     ...config.headers,
+    'X-CSRF-KEY': process.env.BREEZE_NEXT_CSRF_KEY,
     Authorization: session?.access_token ? `Bearer ${session?.access_token}` : '',
   }
   config = { headers, ...config }
